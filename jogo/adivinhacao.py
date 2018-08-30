aca7c87ece0f868651960e232861374dcd6443e5
tanzarella <tanzarella@gmail.com>
2018-08-30 16:33:38 -0300

    Capitulo 3

--- jogo/adivinhacao.py
+++ jogo/adivinhacao.py
@@ -7,9 +7,11 @@ numero_secreto = 42
 chute = int(input("Digite o seu número: "))
 print("Você digitou: ", chute)
 
-if (numero_secreto == chute):
-    print("Você acertou!")
+if numero_secreto > chute:
+    print("Você errou! O seu chute foi menor que o número secreto.")
+elif numero_secreto < chute:
+    print("Você errou! O seu chute foi maior que o número secreto.")
 else:
-    print("Você errou!")
+    print("Você acertou!")
 
 print("Fim do jogo")
