26155643af779e68595d10582c678b9c087ba38c
tanzarella <tanzarella@gmail.com>
2018-08-30 17:18:44 -0300

    Capitulo 4

--- jogo/adivinhacao.py
+++ jogo/adivinhacao.py
@@ -3,15 +3,22 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+tentativa = 1
 
-chute = int(input("Digite o seu número: "))
-print("Você digitou: ", chute)
+while tentativa <= total_de_tentativas:
+    print("Tentativa {} de {}".format(tentativa,total_de_tentativas))
+    chute = int(input("Digite o seu número: "))
+    print("Você digitou: ", chute)
+    
+    if numero_secreto > chute:
+        print("Você errou! O seu chute foi menor que o número secreto.")
+    elif numero_secreto < chute:
+        print("Você errou! O seu chute foi maior que o número secreto.")
+    else:
+        print("Você acertou!")
+        tentativa = total_de_tentativas
 
-if numero_secreto > chute:
-    print("Você errou! O seu chute foi menor que o número secreto.")
-elif numero_secreto < chute:
-    print("Você errou! O seu chute foi maior que o número secreto.")
-else:
-    print("Você acertou!")
-
+    tentativa += 1
+    
 print("Fim do jogo")
